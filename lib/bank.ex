e1a14d8d6d0929f444b2a7a60b58bdefb0a7ea8a
Simon Schulz <simon.schulz@active-group.de>
2018-06-21 21:30:27 +0200

    Add proper data handling

--- lib/bank.ex
+++ lib/bank.ex
@@ -10,10 +10,10 @@ defmodule Bank do
 
   @spec create(String.t) :: :ok
   def create(name), do: notify(%CreateAction{name: name})
-    
+
   @spec deposit(String.t, float()) :: :ok
   def deposit(name, amount), do: notify(%DepositAction{name: name, amount: amount})
-  
+
   @spec withdraw(String.t, float()) :: :ok
   def withdraw(name, amount), do: notify(%WithdrawAction{name: name, amount: amount})
 
@@ -24,23 +24,23 @@ defmodule Bank do
   def start_link(name), do: GenServer.start_link(__MODULE__, name, name: name)
 
   def start(name) do
-      GenServer.start(__MODULE__, name, name: name)
+    GenServer.start(__MODULE__, name, name: name)
   end
 
-  
+
   @spec init(atom()) :: {:ok, map()}
   def init(name) do
-      {:ok, balance} = initial_balances(name)
-      subscribe(name)
-      {:ok, balance}
-  end 
+    {:ok, balance} = initial_balances(name)
+    subscribe(name)
+    {:ok, balance}
+  end
 
   @spec initial_balances(atom()) :: {:ok, map()}
   defp initial_balances(my_name) do
-      case :gproc.lookup_pids({:p, :l, :banks}) do
-          [first | _] -> GenServer.call(first, %GetBalancesAction{})
-          _ -> {:ok, %{}}
-      end
+    case :gproc.lookup_pids({:p, :l, :banks}) do
+        [first | _] -> GenServer.call(first, %GetBalancesAction{})
+        _ -> {:ok, %{}}
+    end
   end
 
   @spec subscribe(atom()) :: true
@@ -52,26 +52,49 @@ defmodule Bank do
   defp notify(action), do: :gproc.send({:p, :l, :banks}, action)
 
 
-  @spec handle_info(action :: BankActions.alter, state :: map()) :: {:noreply, map()} 
+  @spec handle_info(action :: BankActions.alter, state :: map()) :: {:noreply, map()}
 
   def handle_info(%CreateAction{name: name}, balances) do
-      {:noreply, Map.put(balances, name, 0)}
+    new_state = if Map.has_key?(balances, name) do
+        balances
+    else
+        Map.put(balances, name, 0)
+    end
+    {:noreply, new_state}
   end
 
   def handle_info(%DepositAction{name: name, amount: amount}, balances) do
-      {:noreply, Map.update(balances, name, nil, fn balance -> balance + amount end)}
+    old_balance = Map.get(balances, name)
+    new_state = if old_balance do
+        Map.put(balances, name, old_balance + amount)
+    else
+        balances
+    end
+    {:noreply, new_state}
   end
-  
+
+
   def handle_info(%WithdrawAction{name: name, amount: amount}, balances) do
-      {:noreply, Map.update(balances, name, nil, fn balance -> balance - amount end)}
+    old_balance = Map.get(balances, name)
+    new_state = if old_balance do
+        Map.put(balances, name, old_balance - amount)
+    else
+        balances
+    end
+    {:noreply, new_state}
   end
 
 
   @spec handle_call(BankActions.info, any, map()) :: {:reply, {:ok, float() | map()}, map()}
 
   def handle_call(%BalanceAction{name: name}, _from, balances) do
-      %{^name => balance} = balances
-      {:reply, {:ok, balance}, balances}
+    balance = Map.get(balances, name)
+    reply = if balance do
+        {:ok, balance}
+    else
+        {:error, :not_found}
+    end
+    {:reply, reply, balances}
   end
 
   def handle_call(%GetBalancesAction{}, _from, balances) do
